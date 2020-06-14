145136cd5c72895def749ddc0a19638054dfe04d
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-14 06:33:51 +0200

    Set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Header from '../src/components/Header';
+import HomePage from '../src/pages/HomePage';
+import LoginPage from '../src/pages/LoginPage';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" exact component={LoginPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+    </div>
+  );
 }
