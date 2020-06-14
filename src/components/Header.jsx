b60317fd86a3c2f3e93dc449c9fa6b9b2282a05c
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-14 07:23:48 +0200

    Add styling and animation to Link to HomePage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,67 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styled, { keyframes } from 'styled-components';
+
+import SVG from './SVG';
 export default function Header() {
   return (
     <div>
-      <Link to="/">Home</Link>
+      <SVG />
+
+      <Logo>
+        <StyledLink to="/">Home</StyledLink>
+      </Logo>
+
       <Link to="/login">Login</Link>
     </div>
   );
 }
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
+
+const moveIn = keyframes`
+  0% {
+    left: 400px;
+    color: #f3f3f3;
+    box-shadow: none;
+  }
+  70% {
+    left: 0.1rem;
+    color: #f3f3f3;
+    box-shadow: none;
+  }
+  100% {
+    left: 1.6rem;
+    color: #044d8c;
+    box-shadow: inset 8px 8px 8px #cbced1, inset -8px -8px 8px #fff;
+  }
+`;
+
+const Logo = styled.div`
+  width: 12rem;
+  height: 12rem;
+  font-size: 3rem;
+  background-color: #f3f3f3;
+  border-radius: 50%;
+  font-weight: 700;
+  position: absolute;
+  top: 1.6rem;
+  left: 1.6rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+  animation: ${moveIn} 2s forwards ease-out;
+`;
---
+++ src/components/SVG.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function SVG() {
+  return (
+    <div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 600">
+          <path
+            fill="#07B2D9"
+            fillOpacity="1"
+            d="M 0 0 L 550 0 Q 50 50 0 500 L 0 0 "
+          ></path>
+
+          <path
+            fill="#068BBF"
+            fillOpacity="1"
+            d="M 0 0 L 350 0 Q 50 50 0 400 L 0 0 "
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+}
