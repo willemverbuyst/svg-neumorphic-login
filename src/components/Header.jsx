ebb0b7b0e697bf91e2245bd59ab619cbe6681792
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-14 08:56:51 +0200

    Add styling to login page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,18 +8,44 @@ export default function Header() {
   return (
     <div>
       <SVG />
-
-      <Logo>
+      <LogoLink>
         <StyledLink to="/">Home</StyledLink>
-      </Logo>
-
-      <Link to="/login">Login</Link>
+      </LogoLink>
+      <LoginLink>
+        <StyledLink to="/login">Login</StyledLink>
+      </LoginLink>
     </div>
   );
 }
 
+const moveIn = keyframes`
+  0% {
+    left: 400px;
+    box-shadow: none;
+  }
+  70% {
+    left: 0.3rem;
+    box-shadow: none;
+  }
+  100% {
+    left: 2.4rem;
+    box-shadow: inset 8px 8px 8px #cbced1, inset -8px -8px 8px #fff;
+  }
+`;
+
+const changeFontColor = keyframes`
+  0% {
+    color: #f3f3f3;
+  }
+  100% {
+    color: #044d8c; 
+  }
+`;
+
 const StyledLink = styled(Link)`
   text-decoration: none;
+  animation: ${changeFontColor} 2s forwards ease-out;
+  transition: 0.4s ease-in-out;
 
   &:focus,
   &:hover,
@@ -28,40 +54,43 @@ const StyledLink = styled(Link)`
   &:active {
     text-decoration: none;
   }
-`;
 
-const moveIn = keyframes`
-  0% {
-    left: 400px;
-    color: #f3f3f3;
-    box-shadow: none;
-  }
-  70% {
-    left: 0.1rem;
-    color: #f3f3f3;
-    box-shadow: none;
-  }
-  100% {
-    left: 1.6rem;
-    color: #044d8c;
-    box-shadow: inset 8px 8px 8px #cbced1, inset -8px -8px 8px #fff;
+  &:hover {
+    transform: scale(1.1);
   }
 `;
 
-const Logo = styled.div`
-  width: 12rem;
-  height: 12rem;
-  font-size: 3rem;
+const LogoLink = styled.div`
+  width: 18rem;
+  height: 18rem;
+  font-size: 4.5rem;
   background-color: #f3f3f3;
   border-radius: 50%;
   font-weight: 700;
   position: absolute;
-  top: 1.6rem;
-  left: 1.6rem;
+  top: 2.4rem;
+  left: 2.4rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   z-index: 10;
   animation: ${moveIn} 2s forwards ease-out;
+`;
+
+const LoginLink = styled.div`
+  width: 8rem;
+  height: 8rem;
+  font-size: 2rem;
+  background-color: #f3f3f3;
+  border-radius: 50%;
+  border: 1px solid #07b2d9;
+  font-weight: 700;
+  position: absolute;
+  top: 2.4rem;
+  right: 2.4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,96 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log('clicked login button');
+  };
+
+  return (
+    <div>
+      <Main>
+        <form>
+          <div>
+            <Input type="username" placeholder="username" />
+          </div>
+          <div>
+            <Input type="password" placeholder="password" />
+          </div>
+          <div>
+            <Button onClick={(e) => handleClick(e)}>Login</Button>
+          </div>
+
+          <Links>
+            <Llink to="/">Forgot password?</Llink> or{' '}
+            <Llink to="/">Sign up</Llink>
+          </Links>
+        </form>
+      </Main>
+    </div>
+  );
 }
+
+const Main = styled.main`
+  position: absolute;
+  top: 5rem;
+  margin: auto;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  width: 40vw;
+  border: none;
+  outline: none;
+  background: none;
+  font-size: 1.8rem;
+  padding: 2rem 2rem;
+  color: inherit;
+  margin-bottom: 3rem;
+  border-radius: 3rem;
+  box-shadow: inset 8px 8px 8px #cbced1, inset -8px -8px 8px #fff;
+
+  &::placeholder {
+    color: #068bbf;
+  }
+`;
+
+const Button = styled.button`
+  margin-top: 1rem;
+  margin-bottom: 3rem;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  width: 100%;
+  height: 6rem;
+  border-radius: 3rem;
+  font-size: 2rem;
+  font-family: inherit;
+  color: #044d8c;
+  background-color: #f3f3f3;
+  box-shadow: 3px 3px 8px #b1b1b1, -3px -3px 8px #fff;
+  transition: 0.5s;
+
+  &:hover {
+    box-shadow: 2px 2px 5px #b1b1b1, -2px -2px 5px #fff;
+  }
+  &:active {
+    box-shadow: inset 1px 1px 3px #cbced1, inset -1px -1px 3px #fff;
+  }
+`;
+
+const Links = styled.div`
+  padding-top: 2rem;
+`;
+
+const Llink = styled(Link)`
+  text-decoration: none;
+  color: #068bbf;
+  font-size: 1.5rem;
+`;
